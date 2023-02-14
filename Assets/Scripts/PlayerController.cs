7d4a8c9bd26695cbed4d08eea9a4d004cfc05d6e
Jesus Noland <jesus.noland@litpen.com>
2023-02-13 22:05:21 -0800

    Adding player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,21 @@ public class PlayerController : MonoBehaviour
 {
     // This component will handle player action and state
     public int health = 100;
+    public float speed = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Debug.Log("Hi");
-        transform.Translate(Vector3.forward * Time.deltaTime);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
+        transform.Translate(verticalInput * speed * Time.deltaTime * Vector3.up);
     }
 }
