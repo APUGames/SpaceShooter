d35861e88180dc0cb51263014c6f96c17b5faee7
Jesus Noland <jesus.noland@litpen.com>
2023-02-19 17:36:02 -0800

    Updating the player's translation to use less lines

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // This component will handle player action and state
-    public int health = 100;
+
+    [SerializeField]
+    private int health = 100;
+
     public float speed = 1.0f;
 
     // Start is called before the first frame update
@@ -17,10 +20,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Get X-axis movement
         float horizontalInput = Input.GetAxis("Horizontal");
+
+        // Get Y-axis movement
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
-        transform.Translate(verticalInput * speed * Time.deltaTime * Vector3.up);
+        // Deprecated: Using a more performant statement
+        // transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
+        // transform.Translate(verticalInput * speed * Time.deltaTime * Vector3.up);
+
+        // Create direction Vector3 object
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0f);
+
+        // Move the player game object
+        transform.Translate(direction * speed * Time.deltaTime);
     }
 }
